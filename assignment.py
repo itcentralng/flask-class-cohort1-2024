55e320aa20d1148ec2013671db702f5dc303dbda
Nasir Mustapha <teboy.com@gmail.com>
2024-05-21 10:57:31 +0100

    assingment 2

--- assignment.py
+++ assignment.py
@@ -3,3 +3,9 @@
 1. Virtual Environment
 2. 5 Endpoints each describing you
 """
+
+"""
+1. List out the frequenlty used HTTP Methods and explain what they do.
+2. Create an app and return a html string for example: '<h1>My name is:</h1>Kundil'. 
+You can get your HTML samples from anywhere you want
+"""
